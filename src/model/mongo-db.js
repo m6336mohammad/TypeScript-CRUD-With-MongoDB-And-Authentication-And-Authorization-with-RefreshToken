e1bd08c8973176ac5e346270fd3c782f2083a5af
minakarimi1 <minakarimi1@yahoo.com>
2023-11-07 15:54:18 +0330

    added schema

--- src/model/mongo-db.js
+++ src/model/mongo-db.js
@@ -3,3 +3,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
 .then(()=>console.log("connect to database"))
 .catch((e)=>console.log("could not connect to database"+e))
 
+const userSchema = new mongoose.Schema ({
+  name: {type: String},
+  email: {type: String, require:true},
+  password: {type: String, require: true},
+});
+const User = mongoose.model('User',userSchema);
