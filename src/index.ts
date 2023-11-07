942c59c4a145fd7efda4c685a63fc8626e649880
mohammad <m6336.programer@gmail.com>
2023-11-07 11:50:00 +0330

    added server config

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-let x:number = 10;
-console.log(x);
+import 'dotenv'
+import express from "express";
+const app = express()
+const port  = process.env.PORT || 3000;
+
+app.use(express.json())
+
+
+
+
+app.listen(port,()=>{
+  console.log(`Server run on port ${port}`);
+})
