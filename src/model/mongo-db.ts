44a8cb680a5862b6d8470c07f0a81eb21c55b44b
mohammad <m6336.programer@gmail.com>
2023-11-08 09:36:42 +0330

    split connection and modle

--- src/model/mongo-db.ts
+++ src/model/mongo-db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
-.then(()=>console.log("connect to database"))
-.catch((e: string)=>console.log("could not connect to database"+e))
+// mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
+// .then(()=>console.log("connect to database"))
+// .catch((e: string)=>console.log("could not connect to database"+e))
 
 const userSchema = new mongoose.Schema ({
   name: {type: String},
