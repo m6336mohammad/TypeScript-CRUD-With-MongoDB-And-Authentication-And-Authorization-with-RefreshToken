c10e64433048e06fc28a99328639a058d5cee47e
mohammad <m6336.programer@gmail.com>
2023-11-09 04:44:29 +0330

    delete file

--- src/mongoDB.ts
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-const pool = mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
-.then(()=>console.log("connect to database"))
-.catch((e: string)=>console.log("could not connect to database"+e))
-
-export default pool
