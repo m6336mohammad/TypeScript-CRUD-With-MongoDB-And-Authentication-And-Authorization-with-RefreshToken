4a9bab559f056ba1180409274e569ab008cbc1db
mohammad <m6336.programer@gmail.com>
2023-11-09 04:44:51 +0330

    added mongoDB connection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config'
 import express from "express";
-const app = express()
+import mongoose from "mongoose";
 const port  = process.env.PORT || 3000;
+const app = express()
+
+mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
+.then(()=>console.log("connect to database"))
+.catch((e: string)=>console.log("could not connect to database"+e))
 
 app.use(express.json())
+
+
+
 
 
 
