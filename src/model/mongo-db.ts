810d8cb99839333146c874e682103173e3acbab9
mohammad <m6336.programer@gmail.com>
2023-11-09 04:45:37 +0330

    remove connection

--- src/model/mongo-db.ts
+++ src/model/mongo-db.ts
@@ -1,7 +1,3 @@
-import mongoose from "mongoose";
-// mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
-// .then(()=>console.log("connect to database"))
-// .catch((e: string)=>console.log("could not connect to database"+e))
 
 const userSchema = new mongoose.Schema ({
   name: {type: String},
