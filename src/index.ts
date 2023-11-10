32874a15eca5d48270099a2a8a170f0ef50bfe33
mohammad hadi <147169201+m6336mohammad@users.noreply.github.com>
2023-11-11 02:30:16 +0330

    Update index.ts
    added body parser

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 const port  = process.env.PORT || 3000;
 const app = express()
+app.use(express.json())
 
 mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
 .then(()=>console.log("connect to database"))
