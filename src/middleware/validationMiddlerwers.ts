c75bab250361db99fe04d4a7b72a6d620f7a9e79
Mohammad hadi <m6336.programer@gmail.com>
2023-11-17 18:28:56 +0330

    added validatorMiddlerwers.ts

---
+++ src/middleware/validationMiddlerwers.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+import { plainToInstance } from "class-transformer";
+import { validate } from "class-validator";
+import ClientErros from "../errors/clientError";
+const validateMiddlerwers = (validaionSchema: any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const body = req.body;
+    // const errors = {};
+    const clientErros = new ClientErros
+    const validaionClass = plainToInstance(validaionSchema, body);
+    validate(validaionClass, {}).then((error) => {
+      if (error.length > 0) {
+        clientErros.data = []
+        clientErros.errors = error.map((errors:any)=>{
+            return {[errors.property]:Object.values(errors.constraints)}
+        });
+        res.status(400).json(clientErros);
+        console.log(clientErros.errors)
+      } else {
+        next();
+      }
+    });
+  };
+};
+export default validateMiddlerwers;
