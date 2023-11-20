d78c9a81c71a55fe2e7f0161861c8f4304b4eb1e
Mohammad hadi <m6336.programer@gmail.com>
2023-11-20 15:39:49 +0330

    addes server connect ofther database connect

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
-import homeRouter from './routes/home-route';
-import userRouter from './routes/user-router';
+import homeRouter from "./routes/home-route";
+import userRouter from "./routes/user-router";
 import mongoose from "mongoose";
-const port  = process.env.PORT || 3000;
-const app = express()
-app.use(express.json())
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/usearInfo')
-.then(()=>console.log("connect to database"))
-.catch((e: string)=>console.log("could not connect to database"+e))
+app.use(express.json());
+app.use("/", homeRouter);
+app.use("/users", userRouter);
 
-app.use(express.json())
-app.use('/',homeRouter)
-app.use('/users',userRouter)
-
-
-
-
-
-
-
-app.listen(port,()=>{
-  console.log(`Server run on port ${port}`);
-})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/usearInfo")
+  .then(() => {
+    console.log("connect to database");
+    app.listen(port, () => {
+      console.log(`Server run on port ${port}`);
+    });
+  })
+  .catch((e: string) => console.log("could not connect to database" + e));
