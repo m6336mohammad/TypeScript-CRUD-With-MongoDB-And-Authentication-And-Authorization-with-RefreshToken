92ce2b9f4f47c6e72cf00c0ef21ec4404f0ee8e3
minakarimi1 <minakarimi1@yahoo.com>
2023-11-21 14:04:29 +0330

    added get in userService

---
+++ src/users/userController.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from "express";
+import { authMiddleware, validateMiddlerwers } from "./../middleware";
+import { getAllUsers } from "./userService";
+
+const router = Router();
+
+router.get("/", authMiddleware, async (req: Request, res: Response) => {
+  try {
+    res.send(getAllUsers());
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+});
