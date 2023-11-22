4e56b2482a247205a02145030a2ab86da7f48ab8
Mohammad hadi <m6336.programer@gmail.com>
2023-11-22 04:45:08 +0330

    added getAllUsers() Promise

--- src/users/userService.ts
+++ src/users/userService.ts
@@ -1,5 +1,28 @@
+import UserModel from "../model/userModel";
 
 //get AllUsers
 export const getAllUsers = () => {
-  return "you get all users"
+  return new Promise((resolve, rejects) => {
+    UserModel.find()
+      .then((users) => {
+        resolve(users);
+      })
+      .catch((err) => {
+        rejects(err);
+      });
+  });
+
+};
+
+//get User By Id
+export const getUserByID = async (id: string) => {
+  return new Promise((resolve, rejects) => {
+    UserModel.findById(id)
+      .then((user) => {
+        resolve(user);
+      })
+      .catch((err) => {
+        rejects(err);
+      });
+  });
 };
