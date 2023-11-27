c86e183e6c9f5d747ac54aecc4bffb91226f6365
Mohammad hadi <m6336.programer@gmail.com>
2023-11-28 03:05:36 +0330

    added register service

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -4,3 +4,16 @@ import bcrypt from "bcrypt";
 import { encodeToken , decodeToken } from "../utils";
 import _ from "lodash"
 import ServerError from "../errors/serverError";
+
+export const register = async (data: RegisterDTO) => {
+    const user = await UserModel.findOne({ mobile: data.mobile });
+    if (user) {
+     
+    } else {
+      const hashpassword = await bcrypt.hash(data.password, 10);
+      const newUser = await UserModel.create({ ...data, password: hashpassword });
+      newUser.save();
+      return _.pick(newUser, "mobile","password");
+    }
+  };
+  
