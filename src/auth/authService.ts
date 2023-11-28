88c50539afc9cf7dd2115ac0cc45271fb1a56b9b
Mohammad hadi <m6336.programer@gmail.com>
2023-11-29 02:54:06 +0330

    added login service

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -17,3 +17,23 @@ export const register = async (data: RegisterDTO) => {
     }
   };
   
+  export const login = async (data: LoginDTO) => {
+    //find user by mobile number
+   
+      const user = await UserModel.findOne({ mobile: data.mobile });
+      if (!user) {
+        throw new ServerError(404," not fond user");
+      }
+      const comparePassword = await bcrypt.compare(
+        data.password,
+        `${user.password}`
+      );
+      if (!comparePassword) {
+        throw new ServerError(401,"phone number or password invalid");
+      }
+      //encode user id in database and send to user
+      const token = encodeToken({ id: user._id });
+      return { token: token };
+    
+  };
+  
