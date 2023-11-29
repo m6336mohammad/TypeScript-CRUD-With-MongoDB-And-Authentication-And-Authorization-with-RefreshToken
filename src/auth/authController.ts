4cdcebde43095e51dc5d872e318175100c47867b
minakarimi1 <minakarimi1@yahoo.com>
2023-11-29 17:12:46 +0330

    added router.post in authController

--- src/auth/authController.ts
+++ src/auth/authController.ts
@@ -4,3 +4,16 @@ import { LoginDTO,RegisterDTO } from "./auth_dto";
 import  validateMiddlerwers  from "../middleware/validationMiddlerwers";
 
 const router = Router()
+router.post(
+  "/register",
+  validateMiddlerwers(RegisterDTO),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const body: RegisterDTO = req.body;
+      const newUser = await register(body);
+      res.send(newUser);
+    } catch (err: any) {
+      next(err);
+    }
+  }
+);
