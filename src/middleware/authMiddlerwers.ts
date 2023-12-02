2b153001c62f264f31e980f7f6ca19d7f63ed707
minakarimi1 <minakarimi1@yahoo.com>
2023-12-03 03:29:51 +0330

    added decodeToken

--- src/middleware/authMiddlerwers.ts
+++ src/middleware/authMiddlerwers.ts
@@ -1,19 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import { decodeToken } from "../utils";
 import { RequestWithUser } from "../types/requestWithUser";
-const authMiddleware = (
-  req: RequestWithUser,
-  res: Response,
-  next: NextFunction
-) => {let token = req.headers.authorization;
-  if (!token){
-    return
-    res.status(401).json({ message: "plese login"});
+const authMiddleware = (req: RequestWithUser, res: Response, next: NextFunction) => {
+  let token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "please login" });
   }
   token = token.split(" ")[1];
-  
+  try {
+    const data: any = decodeToken(token);
+    req.user = data.id;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "please login" });
+  }
 };
-
-
 
 export default authMiddleware;
