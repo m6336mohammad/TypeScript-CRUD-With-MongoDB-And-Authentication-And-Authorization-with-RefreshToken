ac53c266cf598bee537cc10c299d37659d0605d4
Mohammad hadi <m6336.programer@gmail.com>
2023-12-02 05:26:49 +0330

    added const authMiddleware

--- src/middleware/authMiddlerwers.ts
+++ src/middleware/authMiddlerwers.ts
@@ -1,3 +1,10 @@
-import {Request,Response,NextFunction} from 'express';
-import { decodeToken } from '../utils';
+import { Request, Response, NextFunction } from "express";
+import { decodeToken } from "../utils";
 import { RequestWithUser } from "../types/requestWithUser";
+const authMiddleware = (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction
+) => {};
+
+export default authMiddleware;
