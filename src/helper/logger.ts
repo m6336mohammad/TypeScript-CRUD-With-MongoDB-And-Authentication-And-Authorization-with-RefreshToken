f11dedbf660ba03a3dbbe6695fb99e4c18979907
mina <minakarimi1@yahoo.com>
2023-12-05 15:24:06 +0330

    createLogger

--- src/helper/logger.ts
+++ src/helper/logger.ts
@@ -5,5 +5,16 @@ export default winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp({ format: "YYYY-MM-DD HH-mm-ss" }),
     winston.format.printf(info => `${info.timestamp} ${info.level} : ${info.message}` ),
-  )
+  ),
+  transports: [
+    new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.printf(info => `${info.timestamp} ${info.level} : ${info.message}` ),
+        )
+    }),
+    new winston.transports.File({filename: "./logs/error.log",level: "error",maxsize:100 * 1024,maxFiles:5}),
+    new winston.transports.File({ filename: "./logs/info.log", level: "info",maxsize:100 * 1024,maxFiles:5 }),
+    new winston.transports.File({ filename: "./logs/warn.log", level: "warn" ,maxsize:100 * 1024,maxFiles:5}),
+  ],
 });
