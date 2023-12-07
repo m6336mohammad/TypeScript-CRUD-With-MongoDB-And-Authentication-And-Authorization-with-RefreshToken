be4304614ad0fe6dd245fb4dd1f5f925227d4399
Mohammad hadi <m6336.programer@gmail.com>
2023-12-07 17:37:37 +0330

    added CreateComponentDTO

---
+++ src/components/dto/componentCreateDTO.ts
@@ -0,0 +1,25 @@
+import { IsDefined, IsEmail, IsOptional, IsPhoneNumber, MinLength} from "class-validator";
+
+class CreateComponentDTO {
+  
+  @IsDefined()
+  title:string
+
+  @IsDefined()
+  description: string;
+  
+  @IsDefined()
+  topic: string;
+  
+  @IsDefined()
+  subscriber: string;
+
+  @IsDefined()
+  publisher: string;
+
+  @IsOptional()
+  user:string
+  
+  @IsOptional()
+  broker:string
+}
