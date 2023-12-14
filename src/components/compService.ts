85a587ff33eaf40c6c0000910cfa6299e9ee6dd0
mina <minakarimi1@yahoo.com>
2023-12-14 15:44:09 +0330

    added getAllComponents

--- src/components/compService.ts
+++ src/components/compService.ts
@@ -3,3 +3,17 @@ import ComponentModel from "../model/ComponentModel";
 import CreateProductsDTO from "./../components/dto/componentCreateDTO"
 import UpdateComponentDTO from "./../components/dto/componentsUpdateDTO";
 import GetAllComponentDTO from "./../components/dto/getAllComponentDTO";
+export const getAllComponents = async (filtter:GetAllComponentDTO) => {
+    const {topic,title,page,page_size}=filtter
+
+    let query = {}
+    if(topic){
+        query={topic:topic}
+    }
+
+    if(title){
+        query={title:title}
+    }
+    const resalt = await ComponentModel.find(query,{},{skip:page_size*(page-1),limit:page_size})
+    return resalt;
+};
