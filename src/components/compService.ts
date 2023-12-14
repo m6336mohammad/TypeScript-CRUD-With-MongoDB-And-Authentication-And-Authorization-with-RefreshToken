24e3e6f7be6999aee67e341777b527e21bf60d52
Mohammad hadi <m6336.programer@gmail.com>
2023-12-14 12:41:35 +0330

    import models

---
+++ src/components/compService.ts
@@ -0,0 +1,5 @@
+import ServerError from "../errors/serverError";
+import ComponentModel from "../model/ComponentModel";
+import CreateProductsDTO from "./../components/dto/componentCreateDTO"
+import UpdateComponentDTO from "./../components/dto/componentsUpdateDTO";
+import GetAllComponentDTO from "./../components/dto/getAllComponentDTO";
