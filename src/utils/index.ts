36d33c4b5299ca58018eee5329e90ac7c23af3ce
Mohammad hadi <m6336.programer@gmail.com>
2023-12-26 21:36:29 +0330

    coments oldd Token method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,12 @@
-import jwt from "jsonwebtoken";
-const SECRET = "secret-key-is-protect";
+// import jwt from "jsonwebtoken";
+// const SECRET = "secret-key-is-protect";
 
-export const encodeToken = (paylod: any) => {
-  const token = jwt.sign(paylod, SECRET, { expiresIn: "30d" });
-  return token;
-};
-export const decodeToken = (token: string) => {
-  const decode = jwt.verify(token, SECRET);
-  return decode;
-};
+// export const encodeToken = (paylod: any) => {
+//   const token = jwt.sign(paylod, SECRET, { expiresIn: "30d" });
+//   return token;
+// };
+// export const decodeToken = (token: string) => {
+//   const decode = jwt.verify(token, SECRET);
+//   return decode;
+// };
+
