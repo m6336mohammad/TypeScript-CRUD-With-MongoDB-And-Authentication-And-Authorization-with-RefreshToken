ad8b8a0fa9514ac6df30daa6d17529ca3c46cec4
Mohammad hadi <m6336.programer@gmail.com>
2023-12-27 18:30:32 +0330

    added encodeTokens

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,27 @@
+// export const encodeToken = (paylod: any) => {
+    //   const token = jwt.sign(paylod, SECRET, { expiresIn: "30d" });
+    //   return token;
+    // };
+    // export const decodeToken = (token: string) => {
+        //   const decode = jwt.verify(token, SECRET);
+        //   return decode;
+        // };
+        
+        
+ 
+        
+
+        
 import jwt from "jsonwebtoken";
 const SECRET = "secret-key-is-protect";
 const REFRESH_SECRET = "refresh-secret-key-is-secure";
 
-// export const encodeToken = (paylod: any) => {
-//   const token = jwt.sign(paylod, SECRET, { expiresIn: "30d" });
-//   return token;
-// };
-// export const decodeToken = (token: string) => {
-//   const decode = jwt.verify(token, SECRET);
-//   return decode;
-// };
-
-
-
+export const encodeTokens = (payload: any) => {
+    // Create an access token with a shorter expiration time (15 minutes)
+    const accessToken = jwt.sign(payload, SECRET, { expiresIn: "15m" });
+  
+    // Create a refresh token with a longer expiration time ( 7 days)
+    const refreshToken = jwt.sign(payload, REFRESH_SECRET, { expiresIn: "2d" });
+  
+    return { accessToken, refreshToken };
+  };
