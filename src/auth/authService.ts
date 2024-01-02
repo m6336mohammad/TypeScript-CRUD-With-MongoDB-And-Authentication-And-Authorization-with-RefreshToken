aa7ed458a551e249e70f9d893d7314d153228ba9
Mohammad hadi <m6336.programer@gmail.com>
2024-01-03 00:15:38 +0330

    added decodeAccessToken, decodeRefreshToken

--- src/auth/authService.ts
+++ src/auth/authService.ts
@@ -1,12 +1,13 @@
 import UserModel from "../model/userModel";
 import { LoginDTO, RegisterDTO } from "./auth_dto";
 import bcrypt from "bcrypt";
-import { encodeToken , decodeToken } from "../utils/index";
+import { encodeTokens , decodeAccessToken,decodeRefreshToken } from "../utils/index";
 import _ from "lodash"
 import ServerError from "../errors/serverError";
 
+//register service
 export const register = async (data: RegisterDTO) => {
-    const user = await UserModel.findOne({ mobile: data.mobile });
+    const user = await UserModel.findOne({ mobile: data.email });
     if (user) {
         throw new ServerError(409,"The mobile number is duplicated");
     } else {
@@ -17,10 +18,11 @@ export const register = async (data: RegisterDTO) => {
     }
   };
   
-  export const login = async (data: LoginDTO) => {
+//login service
+export const login = async (data: LoginDTO) => {
     //find user by mobile number
    
-      const user = await UserModel.findOne({ mobile: data.mobile });
+      const user = await UserModel.findOne({ email: data.email });
       if (!user) {
         throw new ServerError(404," not fond user");
       }
@@ -32,7 +34,7 @@ export const register = async (data: RegisterDTO) => {
         throw new ServerError(401,"phone number or password invalid");
       }
       //encode user id in database and send to user
-      const token = encodeToken({ id: user._id });
+      const token = encodeTokens({ id: user._id });
       return { token: token };
     
   };
