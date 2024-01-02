4666bc5c6c11346c551ec31d2094b37918aff4d9
Mohammad hadi <m6336.programer@gmail.com>
2024-01-03 00:17:12 +0330

    added email dto

--- src/auth/auth_dto/loginDTO.ts
+++ src/auth/auth_dto/loginDTO.ts
@@ -3,7 +3,7 @@ import { IsDefined, IsPhoneNumber, MinLength} from "class-validator";
 export default class LoginDTO {
   @IsPhoneNumber()
   @IsDefined()
-  mobile: string;
+  email: string;
 
   @IsDefined()
   @MinLength(8)
