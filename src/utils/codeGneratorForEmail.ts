0c04f2fb5007d8116d91b67965a6915e3da29e7e
mina <minakarimi1@yahoo.com>
2024-01-05 10:50:15 +0330

    GenerateCode save to db

--- src/utils/codeGneratorForEmail.ts
+++ src/utils/codeGneratorForEmail.ts
@@ -8,3 +8,13 @@ const generateCode = () => {
     return crypto.randomBytes(3).toString('hex');// 6 ragam
 };
 
+// Function to set user reset token and its expiration
+export const codeGeneratorForEmail = async (data:ForgotPasswordDTO) => {
+    const code = generateCode();
+    const user = await UserModel.findOne({ email:data.email });
+
+    if (!user) {
+        throw new Error('User not found');
+    }
+
+};
