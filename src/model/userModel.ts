3ea81f49932fdc4e8608e5ca1402de5526d358e7
Mohammad hadi <m6336.programer@gmail.com>
2024-01-05 08:54:44 +0330

    change userModel.ts items

--- src/model/userModel.ts
+++ src/model/userModel.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema ({
-  name: {type: String},
-  email: {type: String, require:true},
-  password: {type: String, require: true},
-});
-const UserModel = mongoose.model('User',userSchema);
+const userSchema = new mongoose.Schema({
+name:String,
+mobile:String,
+password:String,
+email:String,
+age:Number,
+resetCode: String,
+resetTokenExpiration: Number,
+})
 
-export default UserModel
-
-
+export default mongoose.model('UserModel',userSchema)
+ 
