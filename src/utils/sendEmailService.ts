85e29b15e7f58509c32ad34bb96506714a2c4847
mina <minakarimi1@yahoo.com>
2024-01-07 09:05:46 +0330

    added sendEmailService

---
+++ src/utils/sendEmailService.ts
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer';
+
+export const sendEmailService = async (to:string, message:string,sub:string) =>{
+    const  transporter =  nodemailer.createTransport({
+        service: 'gmail',
+        host:"smtp.gmail.com",
+        port:587,
+        secure:false,
+        auth: {
+            user: '', //google gmail
+            pass: '' //app passwors from google.com
+        }
+    });
+    }
