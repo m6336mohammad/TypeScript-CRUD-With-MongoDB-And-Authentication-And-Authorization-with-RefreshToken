07e01a9a7795e8712130e5e70dbcd50d2c790e7b
Mohammad hadi <m6336.programer@gmail.com>
2024-01-08 00:53:21 +0330

    added  ChangePasswordDTO

--- src/auth/auth_dto/index.ts
+++ src/auth/auth_dto/index.ts
@@ -2,10 +2,12 @@ import LoginDTO from "./loginDTO";
 import RegisterDTO from "./registerDTO";
 import ResetCodeDTO from "./resetCodeDTO";
 import ForgotPasswordDTO from "./forgotPasswordsDTO";
+import { ChangePasswordDTO } from "./changePasswordDTO";
 
 export {
   LoginDTO,
   RegisterDTO,
   ResetCodeDTO,
-  ForgotPasswordDTO
+  ForgotPasswordDTO,
+  ChangePasswordDTO
 }
