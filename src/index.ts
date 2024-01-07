d1cd3b9157d058aee3a342b35a23211bbee912cb
Mohammad hadi <m6336.programer@gmail.com>
2024-01-07 05:47:59 +0330

    added double quotation

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import erroHandellerMiddleware from "./middleware/ErrorHandelingMid";
 import CompController from "./components/compController";
 import logger from "./helper/logger";
 import refreshRouter from "./utils/refreshToken";
-import cors from 'cors'
+import cors from "cors";
 //variable
 const port = process.env.PORT || 3000;
 const app = express();
@@ -15,25 +15,26 @@ const app = express();
 //middlerwers
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 //controller
-app.use("/refresh",refreshRouter)
-app.use('/users',userController)
-app.use('/Components',CompController)
-app.use('/auth',authController)
+app.use("/refresh", refreshRouter);
+app.use("/users", userController);
+app.use("/Components", CompController);
+app.use("/auth", authController);
 
 //erroHandellerMiddleware
-app.use(erroHandellerMiddleware)
-
+app.use(erroHandellerMiddleware);
 
 //database connection
 mongoose
   .connect("mongodb://127.0.0.1:27017/usearInfo")
   .then(() => {
-    console.log("connect to database");
+    logger.info("connect to database");
     app.listen(port, () => {
-      console.log(`Server run on port ${port}`);
+      logger.info(`Server run on port ${port}`);
     });
   })
-  .catch((e: string) => console.log("could not connect to database" + e));
+  .catch((e: string) => {
+    console.error("could not connect to database" + e);
+  });
