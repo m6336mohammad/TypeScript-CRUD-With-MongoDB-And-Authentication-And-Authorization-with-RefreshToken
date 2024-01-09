6ea62a8a1b8f27758f334b1ec215bbc425c36645
mina <minakarimi1@yahoo.com>
2024-01-09 12:17:57 +0330

    added codeGneratorForOTP

--- src/utils/codeGeneratorForOTP.ts
+++ src/utils/codeGeneratorForOTP.ts
@@ -1,8 +1,21 @@
 import crypto from "crypto";
 import userModel from "../model/userModel";
 import {ForgotPasswordDTO} from "../auth/auth_dto";
+import {codeGeneratorForEmail} from "./codeGeneratorForEmail";
+import UserModel from "../model/userModel";
 
 //function to generate a unique token
 const generateCode =()=>{
     return crypto.randomBytes(3).toString('hex'); //6 digits
 };
+
+//function to set user reset token and its expiration
+export const codeGneratorForOTP =async (data:ForgotPasswordDTO)=>{
+const code =generateCode();
+const user =await UserModel.findOne({ email:data.email});
+
+
+
+
+
+}
