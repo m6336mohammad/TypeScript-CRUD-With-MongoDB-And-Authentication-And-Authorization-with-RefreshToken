00d64b176aa5a8658723484656daa76db57b700c
mina <minakarimi1@yahoo.com>
2024-01-09 12:29:59 +0330

    set resetCode,resetTokenExpiration

--- src/utils/codeGeneratorForOTP.ts
+++ src/utils/codeGeneratorForOTP.ts
@@ -1,21 +1,25 @@
 import crypto from "crypto";
-import userModel from "../model/userModel";
 import {ForgotPasswordDTO} from "../auth/auth_dto";
-import {codeGeneratorForEmail} from "./codeGeneratorForEmail";
 import UserModel from "../model/userModel";
 
 //function to generate a unique token
-const generateCode =()=>{
+const generateCode = () => {
     return crypto.randomBytes(3).toString('hex'); //6 digits
 };
 
 //function to set user reset token and its expiration
-export const codeGneratorForOTP =async (data:ForgotPasswordDTO)=>{
-const code =generateCode();
-const user =await UserModel.findOne({ email:data.email});
+export const codeGneratorForOTP = async (data: ForgotPasswordDTO) => {
+    const code = generateCode();
+    const user = await UserModel.findOne({email: data.email});
 
+    if (!user) {
+        throw new Error('User not found')
+    }
 
+    user.resetCode = code;
+    user.resetTokenExpiration = Date.now() + 5 * 60 * 1000; //5 minutes
 
-
+    await user.save();
+    return code;
 
 }
