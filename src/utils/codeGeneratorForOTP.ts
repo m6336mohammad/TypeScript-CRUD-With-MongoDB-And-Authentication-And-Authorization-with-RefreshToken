ec5a71933ea1ea774bcd909c0c4ae40f78750760
mina <minakarimi1@yahoo.com>
2024-01-09 12:10:01 +0330

    generate verify Code with 6 digit

--- src/utils/codeGeneratorForOTP.ts
+++ src/utils/codeGeneratorForOTP.ts
@@ -2,3 +2,7 @@ import crypto from "crypto";
 import userModel from "../model/userModel";
 import {ForgotPasswordDTO} from "../auth/auth_dto";
 
+//function to generate a unique token
+const generateCode =()=>{
+    return crypto.randomBytes(3).toString('hex'); //6 digits
+};
