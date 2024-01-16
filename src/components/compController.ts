544b7d31df9a82679c254106b994720ff48dd6af
mina <minakarimi1@yahoo.com>
2024-01-16 21:09:23 +0330

    edit

--- src/components/compController.ts
+++ src/components/compController.ts
@@ -12,8 +12,8 @@ const router = Router();
 //no requre to authMiddleware becuse all user can be search all product
 router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     try{
-      const filtter : any = req.query
-      const result =await getAllComponents(filtter)
+      const filter : any = req.query
+      const result =await getAllComponents(filter)
       res.status(200).json(result)
     }catch(err){
       next(err)
