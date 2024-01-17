4e7ec330778c8b947b928e0b61b67221c8863d58
mina <minakarimi1@yahoo.com>
2024-01-17 14:09:34 +0330

    edit

--- src/components/compService.ts
+++ src/components/compService.ts
@@ -3,8 +3,8 @@ import ComponentModel from "../model/ComponentModel";
 import CreateProductsDTO from "./../components/dto/componentCreateDTO"
 import UpdateComponentDTO from "./../components/dto/componentsUpdateDTO";
 import GetAllComponentDTO from "./../components/dto/getAllComponentDTO";
-export const getAllComponents = async (filtter:GetAllComponentDTO) => {
-    const {topic,title,page,page_size}=filtter
+export const getAllComponents = async (filter:GetAllComponentDTO) => {
+    const {topic,title,page,page_size}=filter
 
     let query = {}
     if(topic){
